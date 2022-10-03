14f4f44ebb2a6ff8cf2c211d6fbf9d4dabd29e57
emanuelaristimuno <emanuelaristimuno@users.noreply.github.com>
2022-10-03 10:41:44 -0300

    subiendo cambios

---
+++ src/main/java/com/mycompany/githubpackage/Testing.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.githubpackage;
+
+/**
+ *
+ * @author ema_2
+ */
+public class Testing {
+     public static void display(String param){
+        System.out.println("You parameter is " + param);
+    }
+
+    public void someFunc(){
+        System.out.println("Testing.someFunc");
+    }
+}
+
+
